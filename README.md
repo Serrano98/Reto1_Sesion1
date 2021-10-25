759e8ba7b1da450db92ff629ed9aa5eda5390fbc
Serrano98 <maos.isc@gmail.com>
2021-10-25 16:14:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-bgs6ux
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-bgs6ux)
---
+++ index.js
@@ -0,0 +1,3 @@
+// run `node index.js` in the terminal
+var sumar = require('./Sumar');
+sumar(2, 2);
